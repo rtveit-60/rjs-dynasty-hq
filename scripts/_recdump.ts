14a745de8037b6e923d39dfad4ce01047d9becd7
rtveit-60 <roscoe.matthews-tveit@outlook.com>
2026-08-30 21:22:23 -0500

    Remove scratch investigation scripts from the visibility hunt

--- scripts/_recdump.ts
+++
@@ -1,9 +0,0 @@
-import { loadFranchise, mainTable, val } from '../src/main/parser/franchise.ts';
-const fr = await loadFranchise(process.argv[2]);
-const rT = mainTable(fr, 'Recruit');
-await rT.readRecords();
-const rec = rT.records[4101];
-console.log('ALL Recruit fields for Abraham (row 4101):');
-for (const k of Object.keys(rec._fields)) {
-  console.log(`  ${k} = ${String(rec._fields[k].value).slice(0, 44)}`);
-}
